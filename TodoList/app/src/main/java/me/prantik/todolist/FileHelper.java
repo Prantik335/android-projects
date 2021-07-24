0327714062c0fcee0bf11b0f89062592c741687a
Prantik <shahnoorprantik@gmail.com>
2021-07-24 16:19:39 +0600

    TodoList : ok

--- TodoList/app/src/main/java/me/prantik/todolist/FileHelper.java
+++ TodoList/app/src/main/java/me/prantik/todolist/FileHelper.java
@@ -1,4 +1,39 @@
 package me.prantik.todolist;
 
-public class FileHalper {
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileHelper {
+    public static final String FILE_NAME = "listinfo.dat";
+
+    public static void writeData(ArrayList<String> items, Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            ObjectOutputStream oas = new ObjectOutputStream(fos);
+            oas.writeObject(items);
+            oas.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<String> readData(Context context) {
+        ArrayList<String> itemList = new ArrayList<>();
+
+        try {
+            FileInputStream fis = context.openFileInput(FILE_NAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            itemList = (ArrayList<String>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return itemList;
+    }
 }
--- TodoList/app/src/main/java/me/prantik/todolist/MainActivity.java
+++ TodoList/app/src/main/java/me/prantik/todolist/MainActivity.java
@@ -1,14 +1,70 @@
 package me.prantik.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText item;
+    Button add;
+    ListView listView;
+    ArrayList<String> itemList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        item = findViewById(R.id.editText);
+        add = findViewById(R.id.button);
+        listView = findViewById(R.id.list);
+
+        itemList = FileHelper.readData(this);
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, itemList);
+
+        listView.setAdapter(arrayAdapter);
+
+        add.setOnClickListener(view -> addItemToList());
+
+        listView.setOnItemClickListener((adapterView, view, pos, l) -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+            alert.setTitle("Delete");
+            alert.setMessage("Do you want to delete '" + itemList.get(pos) + "' from the list?");
+            alert.setCancelable(false);
+            alert.setNegativeButton("No", (dialogInterface, i1) -> {
+                dialogInterface.cancel();
+            });
+            alert.setPositiveButton("Yes", (dialogInterface, i1) -> {
+                removeItemToList(pos);
+            });
+            AlertDialog alertDialog = alert.create();
+            alertDialog.show();
+        });
+
+    }
+
+    private void addItemToList() {
+        String itemName = item.getText().toString();
+        itemList.add(itemName);
+        item.setText("");
+        arrayAdapter.notifyDataSetChanged();
+        FileHelper.writeData(itemList, this);
+    }
+
+    private void removeItemToList(int pos) {
+        itemList.remove(pos);
+        arrayAdapter.notifyDataSetChanged();
+        FileHelper.writeData(itemList, this);
     }
 }
