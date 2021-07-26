0920f0ecd0c909154412f9705f98685975a19118
Prantik <shahnoorprantik@gmail.com>
2021-07-26 07:27:05 +0600

    Todo List : v1.0 rereleased

--- TodoList/app/src/main/java/me/prantik/todolist/FileHelper.java
+++ TodoList/app/src/main/java/me/prantik/todolist/FileHelper.java
@@ -8,32 +8,36 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class FileHelper {
-    public static final String FILE_NAME = "listinfo.dat";
+public class FileHelper { ;
+    public static final String FILE_NAME = "todos.dat";
 
-    public static void writeData(ArrayList<String> items, Context context) {
+    public static void writeData(List<Map<String, String>> todos, Context context) {
         try {
             FileOutputStream fos = context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
             ObjectOutputStream oas = new ObjectOutputStream(fos);
-            oas.writeObject(items);
+            oas.writeObject(todos);
             oas.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static ArrayList<String> readData(Context context) {
-        ArrayList<String> itemList = new ArrayList<>();
+    public static List<Map<String, String>> readItems(Context context) {
+        List<Map<String, String>> todos = new ArrayList<>();
 
         try {
             FileInputStream fis = context.openFileInput(FILE_NAME);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            itemList = (ArrayList<String>) ois.readObject();
+            todos = (List<Map<String, String>>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        return itemList;
+        return todos;
     }
+
 }
--- TodoList/app/src/main/java/me/prantik/todolist/MainActivity.java
+++ TodoList/app/src/main/java/me/prantik/todolist/MainActivity.java
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText item;
     Button add;
     ListView listView;
-    ArrayList<String> itemList = new ArrayList<>();
-    ArrayAdapter<String> arrayAdapter;
+    SimpleAdapter adapter;
+    List<Map<String, String>> todos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +35,27 @@ public class MainActivity extends AppCompatActivity {
         add = findViewById(R.id.button);
         listView = findViewById(R.id.list);
 
-        itemList = FileHelper.readData(this);
+        todos = FileHelper.readItems(this);
 
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
-                android.R.id.text1, itemList);
+        adapter = new SimpleAdapter(this, todos,
+                android.R.layout.simple_list_item_2,
+                new String[]{"todo", "timestamp"},
+                new int[]{android.R.id.text1, android.R.id.text2});
 
-        listView.setAdapter(arrayAdapter);
+        listView.setAdapter(adapter);
 
         add.setOnClickListener(view -> addItemToList());
 
         listView.setOnItemClickListener((adapterView, view, pos, l) -> {
             AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
             alert.setTitle("Delete");
-            alert.setMessage("Do you want to delete '" + itemList.get(pos) + "' from the list?");
+            alert.setMessage("Do you want to delete '" + todos.get(pos).get("todo") + "' from the list?");
             alert.setCancelable(false);
             alert.setNegativeButton("No", (dialogInterface, i1) -> {
                 dialogInterface.cancel();
             });
             alert.setPositiveButton("Yes", (dialogInterface, i1) -> {
-                removeItemToList(pos);
+                removeTodoFromTodos(pos);
             });
             AlertDialog alertDialog = alert.create();
             alertDialog.show();
@@ -56,15 +65,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void addItemToList() {
         String itemName = item.getText().toString();
-        itemList.add(itemName);
-        item.setText("");
-        arrayAdapter.notifyDataSetChanged();
-        FileHelper.writeData(itemList, this);
+
+        if (itemName.isEmpty()) {
+//            Toast.makeText(this, "Empty", Toast.LENGTH_SHORT).show();
+        }
+        {
+            Map<String, String> todo = new HashMap<>(2);
+            SimpleDateFormat format = new SimpleDateFormat("h:mm a, d'''th' MMM yyy", Locale.getDefault());
+            String currentDateAndTime = format.format(new Date());
+            todo.put("todo", itemName);
+            todo.put("timestamp", currentDateAndTime);
+            todos.add(todo);
+
+            item.setText("");
+
+            adapter.notifyDataSetChanged();
+            FileHelper.writeData(todos, this);
+        }
     }
 
-    private void removeItemToList(int pos) {
-        itemList.remove(pos);
-        arrayAdapter.notifyDataSetChanged();
-        FileHelper.writeData(itemList, this);
+    private void removeTodoFromTodos(int pos) {
+        todos.remove(pos);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(todos, this);
     }
 }
