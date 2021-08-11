5fa5ea6f2a1ac6b4f0527c4528a6cec8b777565f
Prantik <shahnoorprantik@gmail.com>
2021-08-11 10:01:27 +0600

    GN: v1.0 completed

--- GuessingNumber/app/src/main/java/me/prantik/guessingnumber/GameActivity.java
+++ GuessingNumber/app/src/main/java/me/prantik/guessingnumber/GameActivity.java
@@ -2,16 +2,23 @@ package me.prantik.guessingnumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    private static final String TAG = "GameActivity";
 
     private TextView textViewLast, textViewRight, textViewHint;
     private EditText editTextGuess;
@@ -19,6 +26,8 @@ public class GameActivity extends AppCompatActivity {
 
     private int randomNum, digit;
     private int remainingRights = 10;
+
+    private final ArrayList<Integer> userGuesses = new ArrayList<>();
 
     private void initializeViews() {
         // initializing textView(s)
@@ -47,6 +56,9 @@ public class GameActivity extends AppCompatActivity {
         // Generating random number
         generateRandomNum();
 
+        // focus on editTextGuess
+        editTextGuess.requestFocus();
+
         buttonConfirm.setOnClickListener(view -> onConfirm());
 
     }
@@ -61,6 +73,8 @@ public class GameActivity extends AppCompatActivity {
         } else {
             randomNum = random.nextInt(9000) + 1000;
         }
+
+        Log.d(TAG, "Random Number = " + randomNum);
     }
 
     private void onConfirm() {
@@ -71,29 +85,77 @@ public class GameActivity extends AppCompatActivity {
             return;
         }
         // Setting these textViews Visible
-        if(textViewHint.getVisibility() == View.GONE) {
+        if (textViewHint.getVisibility() != View.VISIBLE) {
             textViewHint.setVisibility(View.VISIBLE);
             textViewLast.setVisibility(View.VISIBLE);
+            textViewRight.setVisibility(View.VISIBLE);
         }
-        // parsing userGuess to integer
+        // parsing userGuess to integer then adding to userGuesses
         int userGuess = Integer.parseInt(userGuessStr);
-
+        userGuesses.add(userGuess);
         // Decreasing remainingRights & setting the textView
         remainingRights--;
         textViewRight.setText("Remaining Rights: " + remainingRights);
 
-        if (remainingRights < 0) {
-            Toast.makeText(this, "Game End.", Toast.LENGTH_SHORT).show();
-            finish();
+        if (remainingRights < 1) {
+            finishGame(false);
+            textViewHint.setText("Your guess is wrong!");
         } else if (userGuess > randomNum) {
             textViewHint.setText("Decrease Your Guess!");
         } else if (userGuess < randomNum) {
             textViewHint.setText("Increase Your Guess!");
         } else {
-            Toast.makeText(this, "Correct Guess!", Toast.LENGTH_SHORT).show();
+            finishGame(true);
+            textViewHint.setText("Your guess is correct!");
         }
 
+        // Setting Last guess & editTextGuess to empty
         textViewLast.setText("Last Guess: " + userGuessStr);
+        editTextGuess.setText("");
     }
 
+    private void finishGame(boolean isWon) {
+        String msg;
+        if (isWon) {
+            msg = "Congratulations! You won.\n\n" +
+                    "The number was " + randomNum + ".\n" +
+                    "After " + (10 - remainingRights) + " Attempts, you guessed the number.\n\n" +
+                    "Your guessing numbers : " + userGuesses.toString() + "\n\n" +
+                    "Are you want to play again?\n";
+        } else {
+            msg = "Sorry! You lose.\n\n" +
+                    "The number was " + randomNum + ".\n" +
+                    "You can't guess the correct number.\n\n" +
+                    "Your guessing numbers : " + userGuesses.toString() + "\n\n" +
+                    "Are you want to play again?\n";
+        }
+        // closing the keyboard
+        showOrHideKeyboard(false);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Guessing Number Game");
+        dialog.setMessage(msg);
+        dialog.setPositiveButton("Yes", (dialogInterface, i) -> {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        });
+        dialog.setNegativeButton("No", (dialogInterface, i) -> {
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        });
+        dialog.create().show();
+    }
+
+    public void showOrHideKeyboard(boolean show) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+
+        if(!show) {
+            /* hide keyboard */
+            imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0);
+        } else {
+            /* show keyboard */
+            imm.toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        }
+    }
 }
